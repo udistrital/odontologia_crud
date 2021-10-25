787c19011fe9850daf4a88d29d4ab9b498db6519
BaLuisTo <bryan.luis@segurosbolivar.com>
2021-10-25 15:48:44 -0500

    Documentación terminada

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,11 @@
-// @APIVersion 1.0.0
-// @Title beego Test API
-// @Description beego has a very cool tools to autogenerate documents for your API
-// @Contact astaxie@gmail.com
-// @TermsOfServiceUrl http://beego.me/
-// @License Apache 2.0
-// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+// @APIVersion 1.0
+// @Title Odontologia
+// @Description Api para Odontología dentro del módulo de Salud del proyecto SIBUD
+// @Contact baluist@correo.udistrital.edu.co
+// @TermsOfServiceUrl http://www.udistrital.edu.co/politicas-de-privacidad.pdf
+// @License BSD-3-Clause
+// @LicenseUrl http://opensource.org/licenses/BSD-3-Clause
+// @BasePath /Odontologia/v1
 package routers
 
 import (
